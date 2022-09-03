79a2f31235a9fbd1254c69dcab9c231fbecf3af2
Daniel Mauser <dmauser@hotmail.com>
2022-09-03 14:32:07 -0500

    update

--- vhub-nvafw-bgp/script/linuxrouterbgp.sh
+++ vhub-nvafw-bgp/script/linuxrouterbgp.sh
@@ -9,9 +9,8 @@ sed -i "/net.ipv6.conf.all.forwarding=1/ s/# *//" /etc/sysctl.conf
 asn_quagga=$1
 bgp_routerId=$2
 bgp_network1=$3
-bgp_network2=$4
-routeserver_IP1=$5
-routeserver_IP2=$6
+routeserver_IP1=$4
+routeserver_IP2=$5
 
 sudo apt-get -y update
 
