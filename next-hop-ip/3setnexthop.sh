1282b32dac6a297427a08816227ab79730cbec6d
Daniel Mauser <dmauser@hotmail.com>
2025-04-29 23:05:01 -0500

    update

--- next-hop-ip/3setnexthop.sh
+++ next-hop-ip/3setnexthop.sh
@@ -1,3 +1,18 @@
+# Parameters
+region1=eastus2
+rg=lab-vwan-nexthop
+vwanname=vwan-nexthop
+hub1name=hub1
+
+# Get all vms with nva in the name
+az vm list -g $rg --query "[?contains(name, 'nva')].{Name:name}" -o table
+
 echo Configuring iptables rules on DMZ-NVA...
-scripturi="https://raw.githubusercontent.com/dmauser/azure-vwan-vrf/refs/heads/main/scripts/iptables.sh"
-az vm run-command invoke -g $rg -n dmz-nva --command-id RunShellScript --scripts "curl -s $scripturi | bash" --output none --no-wait
+scripturi="https://raw.githubusercontent.com/dmauser/azure-virtualwan/refs/heads/main/next-hop-ip/scripts/nexthop.sh"
+az vm run-command invoke -g $rg -n spoke2-linux-nva1 --command-id RunShellScript --scripts "curl -s $scripturi | bash" --output none --no-wait
+
+# Get spoke2-nvalb
+
+echo Configuring iptables rules on DMZ-NVA...
+scripturi="https://raw.githubusercontent.com/dmauser/azure-virtualwan/refs/heads/main/next-hop-ip/scripts/nexthop.sh"
+az vm run-command invoke -g $rg -n spoke2-linux-nva1 --command-id RunShellScript --scripts "curl -s $scripturi | bash" --output none --no-wait
--- next-hop-ip/scripts/nexthop.sh
+++ next-hop-ip/scripts/nexthop.sh
@@ -9,6 +9,7 @@ exit
 router bgp 65002
  address-family ipv4 unicast
   neighbor 192.168.1.68 route-map lbnexthop out
+  neighbor 192.168.1.69 route-map lbnexthop out
  exit-address-family
 exit
 write memory
