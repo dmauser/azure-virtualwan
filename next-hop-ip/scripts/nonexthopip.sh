4bf30d4f69172a7668045909b3bbdef2f955d3fe
Daniel Mauser <dmauser@hotmail.com>
2025-04-29 23:34:05 -0500

    Update nonexthopip.sh

--- next-hop-ip/scripts/nonexthopip.sh
+++ next-hop-ip/scripts/nonexthopip.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-ip=$1
 sudo vtysh << EOVTYSH
 conf t
-route-map lbnexthop permit 10
- set ip next-hop $ip
-exit
-
 router bgp 65002
  address-family ipv4 unicast
   no neighbor 192.168.1.68 route-map lbnexthop out
